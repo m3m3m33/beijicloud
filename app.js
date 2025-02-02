eab33f64d6b049813f9fd1d74cd99fc9c1a2059b
beijicloud <132130324+m3m3m33@users.noreply.github.com>
2025-02-02 15:44:19 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 var script = document.createElement('script');
-script.src = "https://assets.salesmartly.com/js/project_22168_22909_1690426892.js";
+script.src = "https://assets.salesmartly.com/js/project_22168_88385_1719767533.js";
 document.body.appendChild(script);
