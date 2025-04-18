7bb71c0de629d8479578c81459fb2a2a50069c6d
beijicloud <132130324+m3m3m33@users.noreply.github.com>
2025-04-18 19:07:17 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-(function() {
-    var s1 = document.createElement("script"),
-        s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/6499486694cf5d49dc5fd9e2/1ifp2vovq';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
-})();
+var script = document.createElement('script');
+script.src = "https://assets.salesmartly.com/js/project_22168_22909_1690426892.js";
+document.body.appendChild(script);
